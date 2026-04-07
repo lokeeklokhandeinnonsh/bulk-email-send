727aa21e096f2630fe19cb09fe80c762c3f59a05
xpertance <xpertance932389@gmail.com>
2026-04-07 15:39:22 +0530

    added render link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://bulk-email-send-6v2r.onrender.com',
         changeOrigin: true
       }
     }
